c8af70ecd68bc342f34da24493abfc1c7bc6621d
sreeprasadls <122106312+sreeprasadls@users.noreply.github.com>
2024-09-06 11:04:23 +0530

    Add files via upload

---
+++ SerialComms.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using UnityEngine.UI;
+using System.Text;
+using System;
+
+public class SerialComms : MonoBehaviour {
+
+    [Header("com port number")]
+    public string com_port_number;
+
+    //serial port
+    private SerialPort serial_port;
+
+    // Use this for initialization
+    void Start(){
+
+        this.init_serial();
+    }
+
+    private void init_serial() {
+
+        try{
+            this.serial_port = new SerialPort(this.com_port_number);
+            this.serial_port.BaudRate = 9600;
+            this.serial_port.Encoding = Encoding.ASCII;
+
+            serial_port.StopBits = StopBits.One;
+            serial_port.DataBits = 8;
+            serial_port.Parity = Parity.None;
+
+            this.serial_port.Open();
+        }
+        catch (Exception e){
+
+            Debug.LogError("init com port error……" + e);
+        }
+
+    }
+
+    public void TriggerFirst() {
+
+        try
+        {
+            this.serial_port.Write("" + 1);
+            //return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error on trigger: " + e);
+            //return false;
+            throw;
+        }
+    }
+
+    public void TriggerSecond() {
+
+        try
+        {
+            this.serial_port.Write("" + 2);
+            //return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error on trigger: " + e);
+            //return false;
+            throw;
+        }
+    }
+
+    public void TriggerThird() {
+
+        try
+        {
+            this.serial_port.Write("" + 3);
+            //return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error on trigger: " + e);
+            //return false;
+            throw;
+        }
+    }
+
+}
---
+++ TelekinesisStep.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NextMind.Examples.Steps
+{
+    public class TelekinesisStep : AbstractStep
+    {
+
+        [SerializeField]
+        private Transform objectA = null;
+
+        /// <summary>
+        /// The current force to apply on the cube.
+        /// </summary>
+        private float strength;
+
+        public override void OnEnterStep()
+        {
+            // Place the cube at the initial position each time this step is activated.
+            //StartCoroutine(SetToInitialPosition());
+
+            // Position objects
+            //objectA.localPosition = Vector3.up * 0.5f;
+            //objectA.localEulerAngles = new Vector3(5, 5, 5);
+        }
+
+        //private IEnumerator SetToInitialPosition(float height = 0.5f, float delay = 0f)
+        //{
+        //    if (delay > 0)
+        //    {
+        //        yield return new WaitForSeconds(delay);
+        //    }
+
+        //    // Reset rigidbody's physics values.
+        //    strength = 0;
+        //    //objectA.velocity = Vector3.zero;
+        //    //objectA.angularVelocity = Vector3.zero;
+
+        //    // Reset transform to the right position.
+        //    objectA.localPosition = Vector3.up * height;
+        //    objectA.localEulerAngles = 5 * Vector3.one;
+        //}
+
+    }
+}
